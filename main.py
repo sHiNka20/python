ff44171c210100b546838deb5d5f5cd06fcebcee
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2024-06-13 09:37:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+data['isRobot'] = data['whoAmI'].apply(lambda x: 1 if x == 'robot' else 0)
+data['isHuman'] = data['whoAmI'].apply(lambda x: 1 if x == 'human' else 0)
+data.drop('whoAmI', axis=1, inplace=True)
+data.head()
+print(data)
